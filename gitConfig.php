3b96a5cf92c2b8af92361c4e73540f056180d5d4
Tobias Lang <t.lang@i6m.de>
2018-11-29 17:55:10 +0100

    delete google api key

--- gitConfig.php
+++ gitConfig.php
@@ -11,9 +11,9 @@ require_once 'src/Github_OAuth_Client.php';
 /*
  * Configuration and setup GitHub API
  */
-$clientID         = 'Iv1.ae6b7174cd275cd6';
-$clientSecret     = '2c3ce5f0f84c9ce222ae7398faeadffd5cf30c61';
-$redirectURL     = 'http://www.gittager.com/';
+$clientID         = '';
+$clientSecret     = '';
+$redirectURL     = '';
 
 $gitClient = new Github_OAuth_Client(array(
     'client_id' => $clientID,
